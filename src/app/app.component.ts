ba7746a63d8bd3189db6d92b5fac4e2c2f36ea25
doctoey <doctoey@odds.team>
2024-03-07 18:54:10 +0700

    Update app.component.html and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-learner';
-  sleeper = 'doctoey'
-  newtitle = ''
-  newsleeper = ''
-  name = 'toeytoeytoey'
+  // title = 'angular-learner';
+  // sleeper = 'doctoey'
+  // newtitle = ''
+  // newsleeper = ''
+  // name = 'toeytoeytoey'
 
-  // สร้าง function ชื่อ changeTitle
-  changeTitle() {
-    this.newtitle = 'Newtitle'
+  // // สร้าง function ชื่อ changeTitle
+  // changeTitle() {
+  //   this.newtitle = 'Newtitle'
+  // }
+
+  // // สร้าง function ชื่อ changeSleeper
+  // changeSleeper() {
+  //   this.newsleeper = 'toeyja'
+  // }
+
+  // changenametosakura(name : string) {
+  //   this.name = name
+  // }
+
+
+  value = 0;
+
+  calbuffet(value: string) {
+    // this.value = value
+    
+    const price = Number(value)
+    this.value = (price*3)/4
   }
 
-  // สร้าง function ชื่อ changeSleeper
-  changeSleeper() {
-    this.newsleeper = 'toeyja'
-  }
-
-  changenametosakura(name : string) {
-    this.name = name
-  }
 }
